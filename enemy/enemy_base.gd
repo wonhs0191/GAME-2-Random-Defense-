0e5f25bacbd39f907d09986b3f39a4a2c596db29
wonhs0191 <wonhs0191@naver.com>
2026-03-21 19:34:59 +0900

    ^_^ enemy base 개발 & enemy code 수정

--- enemy/enemy_base.gd
+++ enemy/enemy_base.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name EnemyBase
 
+var move_speed: float = 50.0          # 이동 속도
+
 func _ready():
 	# 씬 실행 시 default 애니메이션 자동 재생
 	var engine = $Engine
--- enemy/nautolan/frigate/frigate.gd
+++ enemy/nautolan/frigate/frigate.gd
@@ -1,7 +1,4 @@
-extends CharacterBody2D
+extends EnemyBase
 
 func _ready():
-	# 씬 실행 시 default 애니메이션 자동 재생
-	var engine = $Engine
-	if engine:
-		engine.play("default")
+	super._ready()
